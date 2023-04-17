039bf6746ca519b6143ed85ea8d32526c718870e
Raju Sah <raju.sah@happiestminds.com>
2023-04-17 20:46:14 +0530

    added new method ratingHotelFallback tohandle ciruit bracker for fall tallrance

--- UserService/src/main/java/com/micro/user/service/controller/UserController.java
+++ UserService/src/main/java/com/micro/user/service/controller/UserController.java
@@ -2,6 +2,9 @@ package com.micro.user.service.controller;
 
 import com.micro.user.service.entity.User;
 import com.micro.user.service.userService.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     UserService userService;
 
@@ -23,6 +27,7 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
+    @CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User>getSingleUser(@PathVariable String userId){
         User user=userService.getUser(userId);
         return  ResponseEntity.ok(user);
@@ -32,5 +37,14 @@ public class UserController {
        List<User>allUser= userService.getAllUser();
         return ResponseEntity.ok(allUser);
     }
-
+    //Create fall back method for ratingHotelBreaker
+    public ResponseEntity<User>ratingHotelFallback(String userId,Exception ex){
+        logger.info("fallback is executed because service is down",ex.getMessage());
+        User user=User.builder().email("rajukumar.sjce@gmail.com").
+                name("Raju").
+                about("This User is created to inform when Some Services is Down").
+                userId("RAJU-SAH").
+                build();
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
 }
